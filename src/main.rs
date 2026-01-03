1efef17ebdab0f534b2ae26c3b0a4aea45ca99d9
Joseph <21144246+joseph-gio@users.noreply.github.com>
2026-01-03 15:33:47 -0800

    use cargo-metadata and support multiple binaries in same workspace

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 #![doc = include_str!("../README.md")]
 
 use std::collections::BTreeMap;
-use std::ffi::OsStr;
-use std::io::BufRead;
-use std::io::BufReader;
+use std::collections::BTreeSet;
+use std::collections::HashMap;
+use std::collections::btree_map::Entry;
 use std::path::Path;
 use std::path::PathBuf;
-use std::process::Command;
-use std::process::Stdio;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::sync::PoisonError;
 
 use argh::FromArgs;
+use cargo_metadata::MetadataCommand;
+use cargo_metadata::Node;
+use cargo_metadata::NodeDep;
+use cargo_metadata::Package;
+use cargo_metadata::PackageId;
 use log::trace;
 
 /// compute a deterministic hash for a local rust crate
@@ -21,6 +24,10 @@ struct CliArgs {
     /// path to the relevant Cargo.toml file
     #[argh(option)]
     manifest_path: Option<PathBuf>,
+    /// the binary crate for which to create a hash. multiple binaries from the same workspace may be specified.
+    #[argh(option)]
+    #[argh(long = "bin")]
+    bins: Vec<String>,
     /// do not update the Cargo.lock file, erroring if it does not exist or is not up to date
     #[argh(switch)]
     locked: bool,
@@ -35,151 +42,234 @@ fn main() {
 
     let CliArgs {
         manifest_path,
+        bins,
         locked,
     } = argh::from_env();
 
-    let cargo_path = std::env::var_os("CARGO_PATH");
-    let cargo_path = cargo_path.as_deref().unwrap_or(OsStr::new("cargo"));
+    if bins.is_empty() {
+        panic!("did not receive any `bin` flags");
+    }
 
-    let mut cargo_tree =
-        Command::new(cargo_path)
-            .arg("tree")
-            .arg("--all-features")
-            .args(manifest_path.iter().flat_map(|manifest_path| {
-                [OsStr::new("--manifest-path"), manifest_path.as_os_str()]
-            }))
-            .args(locked.then_some("--locked"))
-            .arg("--prefix")
-            .arg("none")
-            .arg("--format")
-            .arg("{p}|{f}|{r}")
-            .stdout(Stdio::piped())
-            .spawn()
-            .expect("failed to start cargo tree");
+    let mut metadata_command = MetadataCommand::new();
+    metadata_command.features(cargo_metadata::CargoOpt::AllFeatures);
+    if let Some(manifest_path) = manifest_path {
+        metadata_command.manifest_path(manifest_path);
+    }
+    if locked {
+        metadata_command.other_options(["--locked".to_owned()]);
+    }
 
-    let mut key_values = BTreeMap::<Key, blake3::Hash>::new();
+    let metadata = metadata_command
+        .exec()
+        .unwrap_or_else(|error| panic!("{error}"));
 
-    let path_dependency_hashes = BTreeMap::<Key, blake3::Hash>::new();
-    let path_dependency_hashes = Arc::new(Mutex::new(path_dependency_hashes));
+    trace!("ran cargo-metadta for {} packages", metadata.packages.len());
+
+    dbg!(metadata.packages.len());
+
+    // package ids corresponding to each of `bins`.
+    let bin_packages = {
+        let arbitrary_placeholder_package = metadata.packages.first().unwrap();
+        let mut package_slots = vec![arbitrary_placeholder_package; bins.len()];
+
+        let mut bin_names_to_slots =
+            std::iter::zip(bins.iter().map(String::as_str), &mut package_slots)
+                .collect::<HashMap<_, _>>();
+
+        let workspace_packages = metadata
+            .packages
+            .iter()
+            .filter(|p| metadata.workspace_members.contains(&p.id));
+        'packages: for package in workspace_packages {
+            for target in &package.targets {
+                dbg!(&target.name);
+                if !target.kind.contains(&cargo_metadata::TargetKind::Bin) {
+                    continue;
+                }
+                if let Some(slot) = bin_names_to_slots.remove(&target.name.as_str()) {
+                    *slot = package;
+                    if bin_names_to_slots.is_empty() {
+                        break 'packages;
+                    }
+                }
+            }
+        }
+
+        if !bin_names_to_slots.is_empty() {
+            let missing_bins = bin_names_to_slots
+                .keys()
+                .copied()
+                .collect::<Vec<_>>()
+                .join(", ");
+            panic!("could not find workspace packages for `{missing_bins}`");
+        }
+
+        package_slots
+    };
+
+    let Some(metadata_resolve) = &metadata.resolve else {
+        panic!("cargo-metadata did not output a resolved dependency graph");
+    };
+
+    let package_nodes = metadata_resolve
+        .nodes
+        .iter()
+        .map(|node| (&node.id, node))
+        .collect::<HashMap<_, _>>();
+
+    let hashes_per_package = Mutex::new(HashMap::<&PackageId, HashedInfo>::new());
 
     rayon::scope(|scope| {
-        read_lines(BufReader::new(cargo_tree.stdout.take().unwrap()), |line| {
-            let line = line.trim_ascii_end();
-            if line.ends_with(b"(*)") {
-                return;
-            }
-
-            let mut fields = line.split(|&b| b == b'|');
-            if let Some(package_ident) = fields.next()
-                && let Some(package_features) = fields.next()
-                && let Some(package_repository) = fields.next()
-            {
-                let mut package_repository = package_repository.trim_ascii();
-
-                let mut package_ident = package_ident
-                    .strip_suffix(b"(proc-macro)")
-                    .unwrap_or(package_ident);
-
-                let mut original_source = None;
-
-                // cargo tree uses a different format for workspace members
-                // or usages of cargo patch
-                if let Some(paren_start) = package_ident.iter().rposition(|&b| b == b'(')
-                    && let Some(paren_end) = package_ident.iter().rposition(|&b| b == b')')
-                {
-                    if !package_repository.is_empty() {
-                        original_source = Some(package_repository);
-                    }
-                    package_repository = package_ident[paren_start + 1..paren_end].trim_ascii();
-                    package_ident = package_ident[..paren_start].trim_ascii_end();
-                }
-
-                let mut key_hasher = blake3::Hasher::new();
-                key_hasher.update(package_ident);
-                let key = Key(key_hasher.finalize());
-
-                let mut value_hasher = blake3::Hasher::new();
-                value_hasher.update(package_features);
-
-                if let Some(original_source) = original_source {
-                    value_hasher.update(original_source);
-                }
-
-                let package_repository = package_repository.trim_ascii_start();
-                if package_repository.starts_with(b"https://")
-                    || package_repository.starts_with(b"http://")
-                {
-                    value_hasher.update(package_features);
-                } else if !package_repository.is_empty() {
-                    // FIXME: we should not require utf8
-                    let repo_path_str =
-                        str::from_utf8(package_repository).expect("invalid utf8 for repo path");
-                    let repo_path = Path::new(OsStr::new(repo_path_str)).to_path_buf();
-                    let path_dependency_hashes = Arc::clone(&path_dependency_hashes);
-                    scope.spawn(move |_| {
-                        trace!(
-                            "Computing hash for path dependency `{}`",
-                            repo_path.display()
-                        );
-                        let hash = hash_directory(&repo_path);
-                        path_dependency_hashes
-                            .lock()
-                            .unwrap_or_else(PoisonError::into_inner)
-                            .insert(key, hash);
-                    });
-                }
-
-                let value = value_hasher.finalize();
-
-                key_values.insert(key, value);
-            } else {
-                panic!("invalid format for line `{line:?}`");
-            }
-        });
-
-        let status = cargo_tree.wait().expect("failed to wait on cargo-tree");
-        if !status.success() {
-            panic!("cargo-tree returned failed status: {status}");
+        for &root_package in &bin_packages {
+            scope.spawn(|scope| {
+                let hashed_info = hash_root_crate(root_package, &package_nodes, scope);
+                hashes_per_package
+                    .lock()
+                    .unwrap()
+                    .insert(&root_package.id, hashed_info);
+            });
         }
     });
 
-    let path_dependency_hashes = path_dependency_hashes
-        .lock()
-        .unwrap_or_else(PoisonError::into_inner);
+    let hashes_per_package = hashes_per_package.into_inner().unwrap();
+    let hashes_per_bin = std::iter::zip(&bins, &bin_packages)
+        .map(|(bin_name, &bin_package)| {
+            let hashed_info = hashes_per_package.get(&bin_package.id).unwrap();
+            let mut hasher = blake3::Hasher::new();
+            hasher.update(hashed_info.first_hash.as_bytes());
 
-    trace!(
-        "Computing final hash from {} crates, including {} path dependencies",
-        key_values.len(),
-        path_dependency_hashes.len()
-    );
+            let path_dependencies = hashed_info.path_dependency_hashes.lock().unwrap();
+            trace!(
+                "collecting hashes from {} path dependencies",
+                path_dependencies.len()
+            );
+            for path_dep_hash in path_dependencies.iter() {
+                hasher.update(path_dep_hash.0.as_bytes());
+            }
 
-    let mut final_hasher = blake3::Hasher::new();
-    for (Key(key), value) in key_values {
-        final_hasher.update(key.as_bytes());
-        final_hasher.update(value.as_bytes());
-    }
-    for value in path_dependency_hashes
-        // key has already been hashed above
-        .values()
-    {
-        final_hasher.update(value.as_bytes());
-    }
-    let final_hash = final_hasher.finalize();
-    println!("{}", final_hash);
+            (bin_name.as_str(), hasher.finalize().to_string())
+        })
+        .collect::<HashMap<_, _>>();
+
+    println!("{}", serde_json::to_string(&hashes_per_bin).unwrap());
 }
 
-fn read_lines(mut reader: impl BufRead, mut f: impl FnMut(&[u8])) {
-    let mut buf = Vec::new();
-    while reader
-        .read_until(b'\n', &mut buf)
-        .expect("failed to read line from cargo tree")
-        != 0
-    {
-        if buf.ends_with(b"\n") {
-            buf.pop();
+struct HashedInfo {
+    first_hash: blake3::Hash,
+    path_dependency_hashes: Arc<Mutex<BTreeSet<Key>>>,
+}
+
+fn hash_root_crate<'scope, 's>(
+    root_package: &Package,
+    packages_to_nodes: &HashMap<&PackageId, &Node>,
+    scope: &'scope rayon::Scope<'s>,
+) -> HashedInfo {
+    let root_node = packages_to_nodes[&root_package.id];
+
+    let mut hasher = blake3::Hasher::new();
+    hasher.update(root_node.id.repr.as_bytes());
+    for feature in &root_node.features {
+        hasher.update(feature.as_bytes());
+    }
+
+    let mut collected_package_hashes = BTreeMap::new();
+
+    let path_dependency_hashes = Arc::new(Mutex::new(BTreeSet::new()));
+
+    collect_deps(
+        Path::new(&root_package.manifest_path).parent().unwrap(),
+        &root_node.deps,
+        packages_to_nodes,
+        &mut collected_package_hashes,
+        &path_dependency_hashes,
+        scope,
+    );
+
+    fn collect_deps<'a, 'scope, 's>(
+        parent_package_path: &Path,
+        deps: &'a [NodeDep],
+        packages_to_nodes: &HashMap<&PackageId, &'a Node>,
+        collected_package_hashes: &mut BTreeMap<&'a PackageId, blake3::Hasher>,
+        path_dependency_hashes: &Arc<Mutex<BTreeSet<Key>>>,
+        scope: &'scope rayon::Scope<'s>,
+    ) {
+        for dep in deps {
+            let node = packages_to_nodes[&dep.pkg];
+
+            match collected_package_hashes.entry(&dep.pkg) {
+                Entry::Vacant(entry) => {
+                    let mut hasher = blake3::Hasher::new();
+                    hasher.update(dep.pkg.repr.as_bytes());
+                    for feature in &node.features {
+                        hasher.update(feature.as_bytes());
+                    }
+                    entry.insert(hasher);
+                }
+                Entry::Occupied(entry) => {
+                    let hasher = entry.into_mut();
+                    for feature in &node.features {
+                        hasher.update(feature.as_bytes());
+                    }
+                    continue;
+                }
+            }
+
+            if let Some(relative_path) = dep.pkg.repr.strip_prefix("path+") {
+                let mut relative_path =
+                    relative_path.strip_prefix("file:").unwrap_or(relative_path);
+
+                if let Some(i) = relative_path.rfind('#') {
+                    relative_path = &relative_path[..i];
+                }
+
+                let package_path = parent_package_path.join(relative_path);
+
+                scope.spawn({
+                    let package_path = package_path.clone();
+                    let path_dependency_hashes = Arc::clone(path_dependency_hashes);
+                    move |_| {
+                        trace!("hashing path dependency `{}`", package_path.display());
+                        let hash = hash_directory(&package_path);
+                        path_dependency_hashes
+                            .lock()
+                            .unwrap_or_else(PoisonError::into_inner)
+                            .insert(Key(hash));
+                    }
+                });
+
+                collect_deps(
+                    &package_path,
+                    &node.deps,
+                    packages_to_nodes,
+                    collected_package_hashes,
+                    path_dependency_hashes,
+                    scope,
+                );
+            } else {
+                collect_deps(
+                    Path::new(""),
+                    &node.deps,
+                    packages_to_nodes,
+                    collected_package_hashes,
+                    path_dependency_hashes,
+                    scope,
+                );
+            }
         }
-        f(&buf);
-        buf.clear();
+    }
+
+    eprintln!(
+        "collecting hash from {} dependencies",
+        collected_package_hashes.len()
+    );
+
+    for node_hasher in collected_package_hashes.values() {
+        hasher.update(node_hasher.finalize().as_bytes());
+    }
+
+    HashedInfo {
+        first_hash: hasher.finalize(),
+        path_dependency_hashes,
     }
 }
 
